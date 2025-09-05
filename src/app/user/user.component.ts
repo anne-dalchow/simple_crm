cf1c40407ea51fc7ae1140bdad2d6eedea93b608
Anne Dalchow <anne.dalchow@web.de>
2025-09-05 13:49:12 +0200

    inject MatDialog and add openDialog methode

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { TooltipPosition } from '@angular/material/tooltip';
+import { A11yModule } from '@angular/cdk/a11y';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, A11yModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {
+  readonly dialog = inject(MatDialog);
+  position: TooltipPosition = 'above';
 
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 }
